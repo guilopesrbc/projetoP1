43e5e39a909e1064b9a2b0b6abb61d9cf2695242
DiogoNogueiraLima <115439066+DiogoNogueiraLima@users.noreply.github.com>
2022-11-03 23:30:21 -0300

    add analise_locais

--- analise_locais.py
+++ analise_locais.py
@@ -1,14 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Análise dos Microdados da Vacina do Covid-19
+
+# In[2]:
+
+
 import pandas as pd
 import matplotlib
-matplotlib.use('TkAgg',force=True)
-from matplotlib import pyplot as plt
-#print("Switched to:",matplotlib.get_backend())
-matplotlib.pyplot.show()
 
 #para pegar a os microdados das vacinas aplicadas 
 microdadosVacina = pd.read_csv("C:\\Users\\diogo\\Downloads\\vacinados.csv", sep=";", encoding='utf-8')
+microdadosVacina
 
-microdadosVacina.head() #para ver as 5 primeiras linhas dos dados
+
+# In[5]:
+
+
+microdadosVacina = microdadosVacina.drop(columns=['cpf','nome','grupo','vacina','lote','data_vacinacao','sexo',])
+microdadosVacina.head()
+
+
+# In[6]:
+
+
+#tirando os valores
+micro = microdadosVacina.dropna()
+
+
+# In[7]:
 
 
 #separando para trabalhar apenas com as colunas de local de vacinaçao e a dose aplicada
@@ -17,15 +37,57 @@ colunasSelecionadas_DosesVacinas = ['dose']
 microdadosVacina_Locais = microdadosVacina.filter(items = colunasSelecionadas_LocaisVacinas)
 microdadosVacina_Doses = microdadosVacina.filter(items = colunasSelecionadas_DosesVacinas)
 
-#fazendo a contagem para ver quantos locais de vacinaçao tem e quantas doses foram aplicadas em cada local - foram 311 locais diferentes
-#print(microdadosVacina_Locais.value_counts())
-#print(microdadosVacina_Doses.value_counts())
 
-#grafico de histograma ta dando erro, ele ta printando no oterminal e nao ta saindo o grafico
-#print(microdadosVacina_Doses.hist())
+# In[8]:
+
+
+#fazendo a contagem para ver quantos locais de vacinaçao tem e quantas doses foram aplicadas em cada local
+microdadosVacina_Locais.value_counts()
+
+
+# In[9]:
+
+
+#fazendo a contagem da distribuiçao de quantas vacinas foram aplicadas de cada dose
+microdadosVacina_Doses.value_counts()
+
+
+# In[13]:
+
+
+#grafico de histograma da quantidade de doses
+microdadosVacina_Doses.hist()
+
+
+# In[15]:
+
 
 #descobrindo a porcentagem de quantas pessoas tomaram cada dose
-print(microdadosVacina_Doses.value_counts())
 distInDoses = microdadosVacina_Doses.value_counts()
 percentInDoses = [100*x/distInDoses.sum() for x in distInDoses]
 print(percentInDoses)
+
+
+# In[2]:
+
+
+
+
+
+# In[2]:
+
+
+
+
+
+# In[2]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
